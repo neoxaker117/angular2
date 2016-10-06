85dd02d51b41c392f846c333459e93e51790a9ac
Taras Ivanov <taras.ivanov@team.wrike.com>
2016-10-06 14:21:34 +0300

    Add angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
+
 import { routing, appRoutingProviders }  from './app.routing.ts';
 import { MainComponent }   from './MainComponent/main.component.ts';
 import { SymbolComponent } from "./SymbolComponent/symbol.component.ts";
@@ -7,6 +9,7 @@ import { SymbolComponent } from "./SymbolComponent/symbol.component.ts";
 @NgModule({
     imports: [
         BrowserModule,
+        MaterialModule.forRoot(),
         routing
     ],
     declarations: [
