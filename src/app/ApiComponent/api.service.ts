1d374da62e7ae4e65973754ec96cc3e82fcec5cf
Taras Ivanov <taras.ivanov@team.wrike.com>
2016-10-10 17:08:56 +0300

    Add list

--- src/app/ApiComponent/api.service.ts
+++ src/app/ApiComponent/api.service.ts
@@ -9,13 +9,24 @@ export class ApiService {
     }
 
     getSymbolById(id: number) {
-
         let symbol: Symbol = new Symbol('New symbol: ' + id);
 
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(symbol);
-            }, 500);
+            resolve(symbol);
+        });
+    }
+
+    getSymbolList() {
+        let symbolList: Array<Symbol> = [
+            new Symbol('symbol 1'),
+            new Symbol('symbol 2'),
+            new Symbol('symbol 3'),
+            new Symbol('symbol 4'),
+            new Symbol('symbol 5'),
+        ];
+
+        return new Promise((resolve, reject) => {
+            resolve(symbolList);
         });
     }
 
--- src/app/ListComponent/list.component.ts
+++ src/app/ListComponent/list.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+
 import { PageService } from "../MainComponent/main.service.ts";
+import { ApiService } from "../ApiComponent/api.service.ts";
+import { Symbol } from "../SymbolComponent/symbol.ts";
 
 @Component({
+    providers: [ApiService],
     selector: 'alphabetr-list',
     templateUrl: 'list.component.html',
 })
 export class ListComponent implements OnInit {
 
-    constructor(private mainService: PageService) {
+    private symbolList: Array<Symbol>;
+
+    constructor(private mainService: PageService,
+                private apiService: ApiService) {
 
     }
 
     ngOnInit() {
+        this.apiService.getSymbolList().then((symbolList: Array<Symbol>) => {
+            this.symbolList = symbolList;
+        });
+
         this.mainService.setPageTitle('List');
     }
 
